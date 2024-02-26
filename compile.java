08c648c81a0728f403b635eb0c752157c27684bc
vikrant-phadtare1101 <vikrant.phadtare@acellere.com>
2024-02-26 15:14:39 +0530

    Update compile.java

--- compile.java
+++ compile.java
@@ -2,6 +2,7 @@ public class compile{
 	static final SecureRandom secureRandom = new SecureRandom();
 
 	 int GenerateRandomValue(){
+		 System.out.print("print");
         return secureRandom.nextInt();
     }
 }
