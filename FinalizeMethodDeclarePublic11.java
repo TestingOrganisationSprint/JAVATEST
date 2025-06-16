443a582f12324dc48ecd41528c52da833187d565
vikrant-phadtare1101 <vikrantph9@gmail.com>
2025-06-16 11:08:02 +0530

    Update FinalizeMethodDeclarePublic11.java

--- FinalizeMethodDeclarePublic11.java
+++ FinalizeMethodDeclarePublic11.java
@@ -4,6 +4,8 @@ import java.applet.Applet;
 public final class FinalizeMethodDeclarePublic1 extends Applet {
 	// EMB-ISSUE: CodeIssueNames.FINALIZE_METHOD_DECLARE_PUBLIC
 	public void finalize() {
+
+		System.out.println("added code issue");
 		} 
 }
 
